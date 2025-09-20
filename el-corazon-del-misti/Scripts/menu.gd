96f7a8674f261f82257669d21622563ef7b3ebd8
AjraHuacsoJeansAnthony <jajra@unsa.edu.pe>
2025-09-20 16:29:24 -0500

    Menu edicion audio

--- el-corazon-del-misti/Scripts/menu.gd
+++ el-corazon-del-misti/Scripts/menu.gd
@@ -1,9 +1,13 @@
 extends Control
 
+var sonidoMenu = preload("res://Audio/mystery-music-loop-226835.mp3")
 
+func _ready():
+	MusicaMenu.stream = sonidoMenu
+	MusicaMenu.play()
+	
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/intro_1.tscn")
-
 
 func _on_opciones_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/opciones.tscn")
