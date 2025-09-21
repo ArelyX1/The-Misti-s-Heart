0038bbf98028da8bab70d5fc70867dd0e25054d8
ArelyX1 <andrycaceres2006@gmail.com>
2025-09-21 02:02:02 -0500

    Guardo cambios locales antes de hacer pull

--- el-corazon-del-misti/Scripts/menu.gd
+++ el-corazon-del-misti/Scripts/menu.gd
@@ -1,8 +1,10 @@
 extends Control
 
 var sonidoMenu = preload("res://Audio/mystery-music-loop-226835.mp3")
+var MusicaMenu
 
 func _ready():
+	MusicaMenu = $MusicaMenu  # Encuentra el nodo recién agregado
 	MusicaMenu.stream = sonidoMenu
 	MusicaMenu.play()
 	
@@ -11,7 +13,6 @@ func _on_button_pressed() -> void:
 
 func _on_opciones_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/opciones.tscn")
-
 
 func _on_salir_pressed() -> void:
 	get_tree().quit()
